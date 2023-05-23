3ae20b2686938b50f0aa1031ab763e1340cab0fe
Cho-ga-nom <uipjo123@naver.com>
2023-05-23 15:05:07 +0900

    검색 API 개선

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -9,17 +9,23 @@ export class Post {
   @Column({ type: 'varchar' })
   writer: string;
 
+  @Column({ default: 0 })
+  category: number;
+
   @Column({ length: 50 })
   title: string;
 
   @Column({ type: 'text' })
   content: string;
 
-  @CreateDateColumn({ type: 'timestamp'})
+  @CreateDateColumn({ type: 'timestamp without time zone' })
   createdAt: Date;
 
   @Column({ default: 0 })
   like: number;
+
+  @Column({ default: 0 })
+  dislike: number;
 
   @OneToMany(type => Comment, comments => comments.post_id, {
     cascade: true,
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Post, Get, Patch, Param } from '@nestjs/common';
+import { Controller, Body, Post, Get, Patch, Param, Logger } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDTO } from './dto/create-post.dto';
 import { UpdatePostDTO } from './dto/update-post-dto';
@@ -6,6 +6,7 @@ import { UpdatePostDTO } from './dto/update-post-dto';
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+  private readonly logger = new Logger(PostController.name);
 
   @Post('create')
   async createPost(@Body() createPostDTO: CreatePostDTO) {
@@ -20,6 +21,16 @@ export class PostController {
   @Get('search_by_writer/:writer')
   async getPostByWriter(@Param('writer') writer: string) {
     return this.postService.getPostByWriter(writer);
+  }
+
+  @Get('search_by_title_content/:input')
+  async getPostByTitleContent(@Param('input') input: string) {
+    return this.postService.getPostByTitleContent(input);
+  }
+
+  @Get('search_by_category/:category_num')
+  async getPostByCategory(@Param('category_num') category_num: number) {
+    return this.postService.getPostByCategory(category_num);
   }
   
   @Patch('update')
