2decd366e7368d3b051ce1935f7b15de01e77fef
Cho-ga-nom <uipjo123@naver.com>
2023-10-02 21:02:10 +0900

    merge

--- src/cache-db/cache-db.service.ts
+++ src/cache-db/cache-db.service.ts
@@ -2,7 +2,6 @@ import { Injectable, Inject, Logger } from '@nestjs/common';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { PlayerPostDTO } from 'src/community/dto/player-post.dto';
-import { RedisClientType } from 'redis';
 
 // 읽기 빈도가 많은 작업에 캐시를 이용
 // 업데이트 작업이 있을 경우에는 캐시 데이터에 씌운 다음 DB에 업데이트
@@ -11,7 +10,6 @@ export class CacheDbService {
   // 생성자 안에 캐시 사용 -> 이를 통해 로직 실행
   constructor(
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
-    @Inject('REDIS_CLIENT') private readonly redis: RedisClientType,
   ) {}
   
   private readonly logger = new Logger(CacheDbService.name);
